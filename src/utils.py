7d162895cba2b3c4754d23193c9acfbebf512113
Bha_Moh <bharath2396@gmail.com>
2025-02-04 22:51:05 +0530

    Log and Exceptionhandling

---
+++ src/utils.py
@@ -0,0 +1,3 @@
+#A utils file in a Python project serves as a central location for storing utility functions and
+# code that can be reused across different parts of the project. The purpose of a utils file is to promote code reusability, maintainability, and organization within a Python project.
+
