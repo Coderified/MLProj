220b9a51612b3cc0fe0fde9fd60f47ea69af759d
Bha_Moh <bharath2396@gmail.com>
2025-02-11 12:09:42 +0530

    EDA & Prob Statement

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,6 @@
 import sys
 
-def error_message_detai(error,error_detail:sys):
+def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info() # will give 3 info
     
     #customexceptionhandling
